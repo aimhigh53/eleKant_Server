a1526fb1695694ab7b3c94f44bed95f3ebb4ca72
Changhwan Son <48404869+Changhwan-Son@users.noreply.github.com>
2020-09-06 19:32:12 +0900

    init commit with initial setting for spring

--- src/main/java/com/eleKant/controller/HomeController.java
+++ src/main/java/com/eleKant/controller/HomeController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
+
+
     @GetMapping("/test")
     public String test(){
         return "test success";
